68b6b1e8063de4d635374afb986529741d9f3993
Alessio Cecconi <alessio.cecconi.1991@gmail.com>
2018-02-19 11:11:46 +0100

    ReactiveMiner: non-declare constraint injection - defined a non-declare constraint A-> <->C \/ <>B [forgotten class in last commit]

---
+++ src/minerful/concept/constraint/nonDeclare/BeforeThisOrLaterThat.java
@@ -0,0 +1,180 @@
+package minerful.concept.constraint.nonDeclare;
+
+import dk.brics.automaton.Automaton;
+import dk.brics.automaton.State;
+import dk.brics.automaton.Transition;
+import minerful.concept.TaskChar;
+import minerful.concept.TaskCharSet;
+import minerful.concept.constraint.Constraint;
+import minerful.concept.constraint.ConstraintFamily;
+import minerful.separated.automaton.ConjunctAutomata;
+import minerful.separated.automaton.SeparatedAutomaton;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement
+public class BeforeThisOrLaterThat extends Constraint {
+	public BeforeThisOrLaterThat(TaskChar param1, TaskChar param2, TaskChar param3) {
+		super();
+		this.parameters.add(new TaskCharSet(param1));
+		this.parameters.add(new TaskCharSet(param2));
+		this.parameters.add(new TaskCharSet(param3));
+		this.base=new TaskCharSet(param1);
+	}
+
+	@Override
+	public String getRegularExpressionTemplate() {
+		return "([^%1$s]*)|([^%1$s]*%1$s[^%2$s]*)|([^%1$s]*%1$s[^%2$s]*%2$s[^%3$s]*%3$s[^%1$s]*)";
+		// "([^A]*)|([^A]*A[^B]*)|([^A]*A[^B]*B[^X]*X[^A]*)";
+	}
+
+	@Override
+	public String getRegularExpression() {
+		return String.format(this.getRegularExpressionTemplate(),
+				this.parameters.get(0).toPatternString(),
+				this.parameters.get(1).toPatternString(),
+				this.parameters.get(2).toPatternString()
+		);
+	}
+
+	@Override
+	public TaskCharSet getImplied() {
+		return null;
+	}
+
+	@Override
+	public Constraint suggestConstraintWhichThisShouldBeBasedUpon() {
+		return null;
+	}
+
+	@Override
+	public Constraint copy(TaskChar... taskChars) {
+		this.checkParams(taskChars);
+		return new BeforeThisOrLaterThat(taskChars[0], taskChars[1], taskChars[2]);
+	}
+
+	@Override
+	public Constraint copy(TaskCharSet... taskCharSets) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkParams(TaskChar... taskChars)
+			throws IllegalArgumentException {
+		return true;
+	}
+
+	@Override
+	public boolean checkParams(TaskCharSet... taskCharSets)
+			throws IllegalArgumentException {
+		return true;
+	}
+
+	@Override
+	public ConstraintFamily getFamily() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T extends ConstraintFamily.ConstraintSubFamily> T getSubFamily() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int compareTo(Constraint t) {
+		int result = super.compareTo(t);
+		if (result == 0) {
+			result = this.getClass().getCanonicalName().compareTo(t.getClass().getCanonicalName());
+		}
+		return result;
+	}
+
+	@Override
+	public boolean isBranched() {
+		return false;
+	}
+
+
+	@Override
+	public SeparatedAutomaton buildParametricSeparatedAutomaton() {
+		char[] alphabet = {'a', 'b', 'c', 'z'};
+		Automaton activator = getSingleCharActivatorAutomaton(alphabet[0], alphabet);
+
+		List<ConjunctAutomata> disjunctAutomata = new ArrayList<ConjunctAutomata>();
+
+//		Eventually in the future B
+		char[] othersFut = {alphabet[0], alphabet[2], alphabet[3]};
+		Automaton futureAutomaton = getEventualityAutomaton(alphabet[1], othersFut);
+		ConjunctAutomata conjunctAutomatonFut = new ConjunctAutomata(null, null, futureAutomaton);
+		disjunctAutomata.add(conjunctAutomatonFut);
+
+//		Eventually in the past C
+		char[] othersPast = {alphabet[0], alphabet[1], alphabet[3]};
+		Automaton pastAutomaton = getEventualityAutomaton(alphabet[2], othersPast);
+		ConjunctAutomata conjunctAutomatonPast = new ConjunctAutomata(pastAutomaton, null, null);
+		disjunctAutomata.add(conjunctAutomatonPast);
+
+		SeparatedAutomaton res = new SeparatedAutomaton(activator, disjunctAutomata, alphabet);
+		res.setNominalID(this.type);
+		return res;
+	}
+
+	/**
+	 * Returns an automaton accepting only if the transition is equal to a specific activator character
+	 *
+	 * @param activator parametric character representing the activator in the parametric automaton
+	 * @param others    all the parametric characters of the alphabet but the activator
+	 * @return activator automaton
+	 */
+	private static Automaton getSingleCharActivatorAutomaton(char activator, char[] others) {
+		State accepting = new State();
+		accepting.setAccept(true);
+
+		State notAccepting = new State();
+
+		notAccepting.addTransition(new Transition(activator, accepting));
+		accepting.addTransition(new Transition(activator, accepting));
+
+		for (char o : others) {
+			notAccepting.addTransition(new Transition(o, notAccepting));
+			accepting.addTransition(new Transition(o, notAccepting));
+		}
+
+		Automaton res = new Automaton();
+
+		res.setInitialState(notAccepting);
+		return res;
+	}
+
+	/**
+	 * Get the automaton representing the <>A eventuality constraint for a desired letter of an alphabet
+	 *
+	 * @param desired desired character
+	 * @param others  alphabet without the desired character
+	 * @return automaton for <>desired
+	 */
+	private static Automaton getEventualityAutomaton(char desired, char[] others) {
+		State NonAcceptingState = new State();
+		State AcceptingState = new State();
+		AcceptingState.setAccept(true);
+
+		NonAcceptingState.addTransition(new Transition(desired, AcceptingState));
+		for (char other : others) {
+			NonAcceptingState.addTransition(new Transition(other, NonAcceptingState));
+		}
+		AcceptingState.addTransition(new Transition(desired, AcceptingState));
+		for (char other : others) {
+			AcceptingState.addTransition(new Transition(other, AcceptingState));
+		}
+
+		Automaton resAutomaton = new Automaton();
+		resAutomaton.setInitialState(NonAcceptingState);
+
+		return resAutomaton;
+	}
+}
